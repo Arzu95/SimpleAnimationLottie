fe2c8cf2cc8ec7bda1d4d80a0f7781c0b761b43a
Arzu95 <muhammadarzu3432@gmail.com>
2024-02-05 12:35:05 +0700

    Initial Commit

---
+++ SimpleAnimationLottie/SimpleAnimationLottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAnimationLottieApp.swift
+//  SimpleAnimationLottie
+//
+//  Created by Muhammad Arzu on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAnimationLottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
